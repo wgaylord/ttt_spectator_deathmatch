3d5703ef53fb434232de2a3f58f05635158d61ad
William Gaylord <chibill110@gmail.com>
2022-10-20 10:23:44 -0500

    Fix error in the AcceptInput handler
    Fixes an issue that occurs on first map load.

--- lua/sv_specdm_overrides.lua
+++ lua/sv_specdm_overrides.lua
@@ -417,8 +417,11 @@ hook.Add("AcceptInput", "AcceptInput_Ghost", function(ent, name, activator, call
 	end
 	if IsValid(ent) and IsValid(caller) and IsValid(activator) then --Make sure all our entites are valid
             if ent:GetClass() == "trigger_multiple" or caller:GetClass() == "trigger_multiple" then --Look for trigger_multiple
-                --print(ent:GetClass(),activator:GetClass(),caller:GetClass())
-                return activator:IsGhost() --Check if ghost if they are stop the activation of them
+                if activator.IsGhost and activator:IsGhost() then
+			return true
+		else
+			reutrn false
+               end
             end
         end		
 end)
